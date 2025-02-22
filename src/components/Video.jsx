b71e999d81aeee3f1257209a18b614731ef35241
Kyle Rego <regoky@outlook.com>
2025-02-22 14:59:20 -0800

    Fix title mistake

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,6 +1,6 @@
 export default function Video({src, w, caption}) {
-    return <div title={src}>
-            <video controls style={{width: w}} autoPlay loop muted playsInline>
+    return <div title={caption}>
+            <video style={{width: w}} autoPlay loop muted playsInline>
                 <source src={src} type="video/mp4" />
             </video>
             <p>
